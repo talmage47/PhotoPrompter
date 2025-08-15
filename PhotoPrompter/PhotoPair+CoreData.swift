447d9b31f610096073134ca84ae6f78c0e2f7dfa
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-08-14 21:21:17 -0600

    Disabled autogenerated class for CoreData

--- PhotoPrompter/PhotoPair+CoreData.swift
+++ PhotoPrompter/PhotoPair+CoreData.swift
@@ -3,10 +3,12 @@ import CoreData
 
 @objc(PhotoPair)
 public class PhotoPair: NSManagedObject {
-    @NSManaged public var id: UUID
-    @NSManaged public var date: Date
-    @NSManaged public var frontImage: Data
-    @NSManaged public var backImage: Data
+    
+    @NSManaged public var id: UUID?
+    @NSManaged public var date: Date?
+    @NSManaged public var prompt: String?
+    @NSManaged public var frontImage: Data?
+    @NSManaged public var backImage: Data?
 }
 
 extension PhotoPair {
